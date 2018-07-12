f71eb58cfe47038e2403e9812e6703d18d1efb88
Palash Mondal <palashm@live.in>
2018-07-12 09:27:44 +0530

    Updated math module

--- src/math.js
+++ src/math.js
@@ -7,14 +7,20 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
+  if (!isPositiveInteger(a)) return 0;
+  if (!isPositiveInteger(b)) return 0;
   return a - b;
 }
 
 function divide(a, b) {
+  if (!isPositiveInteger(a)) return 0;
+  if (!isPositiveInteger(b)) return 0;
   return a / b;
 }
 
 function multiply(a, b) {
+  if (!isPositiveInteger(a)) return 0;
+  if (!isPositiveInteger(b)) return 0;
   return a * b;
 }
 
